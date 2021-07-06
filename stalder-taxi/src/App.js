ada343a8a0fa4e87e4f6795f19de7c31c7aa126d
Lukas Bühler <mail@lukasbuehler.ch>
2021-07-06 22:34:09 +0200

    created react home page and added first build

--- stalder-taxi/src/App.js
+++ stalder-taxi/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AppBar position="static" color="default" elevation={1}>
+          <Toolbar className={"toolbar"}>
+            <div className={"toolbarDiv"}>
+              <Typography variant="h6" color="inherit" noWrap>
+                Stalder Taxi-Service
+              </Typography>
+              <nav className={"navbar"}>
+                <Button variant="text" color="primary" href="/" className={"navToolbarButton"}>
+                  Home
+                </Button>
+                <Button variant="text" color="primary" href="/fleet" className={"navToolbarButton"}>
+                  Flotte
+                </Button>
+                <Button variant="text" color="primary" href="/about" className={"navToolbarButton"}>
+                  Über uns
+                </Button>
+              </nav>
+            </div>
+            <Button className={"navLoginButton"} href="#" color="primary" variant="outlined">
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/fleet">
+            <Fleet />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function About() {
+  return <h2>Über uns</h2>;
+}
+
+function Fleet() {
+  return <h2>Unsere Flotte</h2>;
+}
---
+++ stalder-taxi/src/pages/Home.js
@@ -0,0 +1,34 @@
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+import React from "react";
+
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <div className={"title"}>
+            <Typography variant="h1">Stalder Taxi-Service</Typography>
+            <Typography variant="h4">Der private non-profit Taxi-Service Ihres Vertrauens!</Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img src={process.env.PUBLIC_URL + "/title_image.jpg"} alt="Header Image" className={"titleImage"} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div className={"bookingDiv"}>
+            <Typography variant="h4">Jetzt buchen!</Typography>
+            <Typography variant="body">
+              Wir nehmen zur Zeit keine neuen Passagiere über unsere Website an. Danke für Ihr Verständins.
+            </Typography>
+
+            <Button variant="contained" color="primary" disabled={true}>
+              Buchen
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
